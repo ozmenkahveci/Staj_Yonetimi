973d78535f945252cb555a26ffc136c0ab831307
Özmen Kahveci <80688962+ozmenkahveci@users.noreply.github.com>
2021-05-26 12:43:13 +0300

    Geliştirme V1.1

--- StajYonetim(Kod)/main.c
+++ StajYonetim(Kod)/main.c
@@ -1,10 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+struct ogrenci{
+	char ogrenciAd[25];
+	char ogrenciSoyad[25];
+	int ogrenciNo;
+};
+struct firma{
+	int vergiNo;
+	char firmaAd[25];
+	char faaliyetAlani[25];
+};
+struct staj{
+	int stajFirmaVergiNo;
+};
+
+void ogrenciEkle(struct ogrenci ogrenciBilgisi[],int *i,struct ogrenci *pOgrenci[]){
+	
+	int k=*i;
+	FILE *fp=fopen("Ogrenci.txt","a");
+	
+	if(fp==NULL){
+		printf("Dosya Acilmadi");
+}
+
+	fprintf(fp,"Ogrencinin Adi\t\tOgrencinin Soyadi\t\tOgrencinin Numarasi\n");
+	fprintf(fp,"---------------------------------------------------------------\n");
+	fprintf(fp,"%s\t\t\t%s\t\t\t\t%d\n\n",pOgrenci[k]->ogrenciAd,pOgrenci[k]->ogrenciSoyad,pOgrenci[k]->ogrenciNo);
+	
+	fclose(fp);
+}
+
+void firmaEkle(struct firma firmaBilgisi[],int *f,struct firma *pFirma[]){
+
+	int m=*f;
+	
+	FILE *fp=fopen("Firma.txt","a");
+	
+	if(fp==NULL){
+		printf("Dosya Acilmadi");
+}
+
+	fprintf(fp,"Firmanin Adi\t\tFirmanin faaliyet alani\t\tFirmanin Vergi Numarasý\n");
+	fprintf(fp,"---------------------------------------------------------------------------------\n");
+	fprintf(fp,"%s\t\t\t%s\t\t\t\t%d\n\n",pFirma[m]->firmaAd,pFirma[m]->faaliyetAlani,pFirma[m]->vergiNo);
+
+	fclose(fp);
+}
 
 int main()
 {
     int menu_secim;
-
+    int i=0;
+    int f=0;
+	struct ogrenci ogrenciBilgisi[10];
+	struct firma firmaBilgisi[10];
+	struct staj stajBilgisi[10];
+	
+	struct ogrenci *pOgrenci[10];
+	struct firma *pFirma[10];
+	
     printf("\tStaj Yonetim Programina Hos Geldiniz\n\n");
     anamenu:
     printf("\n\t----------------Menu----------------\n\n");
@@ -29,8 +84,20 @@ int main()
                 switch(menu_secim)
             {
                 case 1:
-
-
+					printf("\n\t-----------Ogrenci Ekleme-----------\n\n");
+					
+					printf("Ogrencinin Adini Giriniz: ");
+					scanf("%s",&ogrenciBilgisi[i].ogrenciAd);
+					printf("Ogrencinin Soydini Giriniz: ");
+					scanf("%s",&ogrenciBilgisi[i].ogrenciSoyad);
+					printf("Ogrencinin Numarasini Giriniz: ");
+					scanf("%d",&ogrenciBilgisi[i].ogrenciNo);
+					pOgrenci[i]=&ogrenciBilgisi[i];
+					
+					ogrenciEkle(ogrenciBilgisi,&i,pOgrenci);
+					
+					i++;
+					
                     goto altbolum1;
                     break;
 
@@ -76,6 +143,19 @@ int main()
             switch(menu_secim)
             {
                 case 1:
+                	printf("\n\t-----------Firma Ekleme-----------\n\n");
+                	
+                	printf("Firmanin ismi: ");
+                	scanf("%s",&firmaBilgisi[f].firmaAd);
+                	printf("Firmanin faaliyet alani: ");
+                	scanf("%s",&firmaBilgisi[f].faaliyetAlani);
+                	printf("Firmanin vergi numarasi: ");
+                	scanf("%d",&firmaBilgisi[f].vergiNo);
+                	
+                	pFirma[f]=&firmaBilgisi[f];
+                	
+                	firmaEkle(firmaBilgisi,&f,pFirma);
+                	
 
 
                     goto altbolum2;
